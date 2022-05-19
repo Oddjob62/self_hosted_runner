80c2dcf98f559b0964e0ccae857233554ffa4b62
Alex Britton <alex@ahbritton.com>
2022-05-19 15:56:56 +0100

    update

---
+++ install-script.sh
@@ -0,0 +1,9 @@
+echo starting
+sudo -i -u runner bash << EOF
+cd ~
+cd actions-runner
+./config.sh --unattended --url https://github.com/replacerepo --token replacetoken --labels replacelabel
+sudo ./svc.sh install
+sudo ./svc.sh start
+# sudo apt -y remove walinuxagent
+# sudo apt -y remove openssh-client
