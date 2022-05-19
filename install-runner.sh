76753f132578f992a66f580b957dfdb54e7db402
Alex Britton <alex@ahbritton.com>
2022-05-19 18:11:08 +0100

    update

--- install-runner.sh
+++ install-runner.sh
@@ -2,9 +2,9 @@
 echo starting
 sudo -i -u runner bash << EOF
 cd ~
-cd actions-runner
+mkdir actions-runner && cd actions-runner
+curl -o actions-runner-linux-x64-2.291.1.tar.gz -L https://github.com/actions/runner/releases/download/v2.291.1/actions-runner-linux-x64-2.291.1.tar.gz
+tar xzf ./actions-runner-linux-x64-2.291.1.tar.gz
 ./config.sh --unattended --url https://github.com/replacerepo --token replacetoken --labels replacelabel
 sudo ./svc.sh install
 sudo ./svc.sh start
-# sudo apt -y remove walinuxagent
-# sudo apt -y remove openssh-client
