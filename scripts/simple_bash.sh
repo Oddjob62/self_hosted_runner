b468ea5c07e293fd6b32fcb350b75e152ddd4997
Alex Britton <alex@ahbritton.com>
2022-05-20 09:24:39 +0100

    Update simple_bash.sh

--- scripts/simple_bash.sh
+++ scripts/simple_bash.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 echo "this has been written via cloud-init" + $(date) >> /tmp/myScript.txt
-sudo useradd -m -G sudo,docker runner
+sudo useradd -m -G sudo runner
 sudo -i -u runner bash << EOF
 cd ~
 mkdir actions-runner && cd actions-runner
