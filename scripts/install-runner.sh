1569e951b15c2c002662f9b9b5daa93385ee03c5
Alex Britton <alex@ahbritton.com>
2022-05-20 09:34:34 +0100

    Update install-runner.sh

--- scripts/install-runner.sh
+++ scripts/install-runner.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
-echo starting
+echo "this has been written via cloud-init" + $(date) >> /tmp/myScript.txt
+sudo useradd -m -G sudo runner
 sudo -i -u runner bash << EOF
 cd ~
 mkdir actions-runner && cd actions-runner
