bf99f8a0ed97a0e4976d44c05116ad7cb194fa85
Alex Britton <alex@ahbritton.com>
2022-05-20 18:49:56 +0100

    Update remove-runnerexemptions.ps1

--- policy-exemptions/remove-runnerexemptions.ps1
+++ policy-exemptions/remove-runnerexemptions.ps1
@@ -16,10 +16,10 @@ param (
   [string]$SecuredPassword
 )
 
-Install-Module -Name 'Az' -Force -Scope CurrentUser
-
 $Credential = New-Object -TypeName System.Management.Automation.PSCredential -ArgumentList $ApplicationId, $SecuredPassword
 Connect-AzAccount -ServicePrincipal -TenantId $TenantId -Credential $Credential
+
+Install-Module -Name 'Az' -Force -Scope CurrentUser
 
 $runnerID = "/subscriptions/ff65e97e-c6be-4f35-b6b7-e52ce88bf5b2/resourceGroups/$resourceGroupName/providers/Microsoft.Compute/virtualMachines/$runnerName"
 
