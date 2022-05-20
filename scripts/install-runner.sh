0ef02ce651d8a24ae5287f4defa5449de984f3c5
Alex Britton <alex@ahbritton.com>
2022-05-20 12:15:16 +0100

    Update install-runner.sh

--- scripts/install-runner.sh
+++ scripts/install-runner.sh
@@ -7,6 +7,12 @@ mkdir actions-runner && cd actions-runner
 curl -o actions-runner-linux-x64-2.291.1.tar.gz -L https://github.com/actions/runner/releases/download/v2.291.1/actions-runner-linux-x64-2.291.1.tar.gz
 tar xzf ./actions-runner-linux-x64-2.291.1.tar.gz
 ./config.sh --unattended --url https://github.com/replacerepo --token replacetoken --labels replacelabel >> input.txt
+
 echo "url replacerepo, token replacetoken, label replacelabel" >> /tmp/config.txt
+
+echo "running ./svc.sh install" >> >>/tmp/output.txt
 sudo ./svc.sh install >>/tmp/output.txt 2>&1
+echo "running ./svc.sh start" >> >>/tmp/output.txt
 sudo ./svc.sh start >>/tmp/output.txt 2>&1
+
+cat /tmp/output.txt
