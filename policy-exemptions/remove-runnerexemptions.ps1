4bbe6694cd20c6d0fd89e098d0b99ba00a1db758
Alex Britton <alex@ahbritton.com>
2022-05-20 18:39:47 +0100

    Update remove-runnerexemptions.ps1

--- policy-exemptions/remove-runnerexemptions.ps1
+++ policy-exemptions/remove-runnerexemptions.ps1
@@ -8,6 +8,8 @@ param (
   [string]$runnerName
 )
 
+Install-Module -Name 'Az' -Force -Scope AllUsers
+
 $runnerID = "/subscriptions/ff65e97e-c6be-4f35-b6b7-e52ce88bf5b2/resourceGroups/$resourceGroupName/providers/Microsoft.Compute/virtualMachines/$runnerName"
 
 Get-AzPolicyExemption -scope $runnerID #| remove-AzPolicyExemption
