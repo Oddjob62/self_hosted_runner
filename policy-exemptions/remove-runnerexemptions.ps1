ad4f8814455d8c610d312d6ed09f928d269cb9f9
Alex Britton <alex@ahbritton.com>
2022-05-20 18:05:14 +0100

    Update remove-runnerexemptions.ps1

--- policy-exemptions/remove-runnerexemptions.ps1
+++ policy-exemptions/remove-runnerexemptions.ps1
@@ -1,15 +1,13 @@
 param (
   [Parameter(Mandatory)]
   [ValidateNotNullOrEmpty()]
-  [string]
-  $resourceGroupName,
+  [string]$resourceGroupName,
  
   [Parameter(Mandatory)]
   [ValidateNotNullOrEmpty()]
-  [string]
-  $runnerName
+  [string]$runnerName
 )
 
-$runnerID = (get-azvm -ResourceGroupName $resourceGroupName -Name $runnerName).id
+$runnerID = "/subscriptions/ff65e97e-c6be-4f35-b6b7-e52ce88bf5b2/resourceGroups/$resourceGroupName/providers/Microsoft.Compute/virtualMachines/$runnerName"
 
 Get-AzPolicyExemption -scope $runnerID | remove-AzPolicyExemption
